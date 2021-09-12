3d605c89f9d466e336c329853bd70e8f888d4b10
kiran rai <raikiran925@gmail.com>
2021-09-12 22:05:10 +0530

    files are added

---
+++ index.js
@@ -0,0 +1,11 @@
+//'use strict';
+//const model = document.querySelector(".share-box");
+//const authorBox = document.querySelector(".author-box");
+//
+//authorBox.addEventListener("click",function(){
+//    authorBox.style.zIndex='-3';
+//    
+//})
+//
+//console.log(model);
+//console.log(authorBox);
